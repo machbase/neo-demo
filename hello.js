a0c736b29f64a7c5bb1d99a437e4547d855067df
Kwon, Yeong Eon <eirny.kwon@machbase.com>
2026-03-31 16:34:33 +0900

    show argv

--- hello.js
+++ hello.js
@@ -1,1 +1,6 @@
-console.println('Hello World?');
+const process = require('process');
+
+console.println(`Hello ${process.argv[2] ? process.argv[2] : 'World'}?`);
+console.println("- argv[0]:", process.argv[0]);
+console.println("- argv[1]:", process.argv[1]);
+console.println("- argv[2]:", process.argv[2]);
